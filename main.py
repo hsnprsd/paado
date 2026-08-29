7807002e0c40b619084177fe52e0063f5268c56c
Ehsan Poursaeed <hsnprsd@gmail.com>
2026-08-29 11:01:59 +0330

    Add flask

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from paado!")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
