bd113c5e3e054050a3bded508315760afe1b2874
Ehsan Poursaeed <hsnprsd@gmail.com>
2026-08-30 11:18:20 +0330

    use headless chrome by default

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ class Config(BaseSettings):
     ollama_host: str = "http://127.0.0.1:11434"
     context_length: int = 64 * 1024
     chrome_path: str = "/usr/bin/google-chrome"
-    chrome_headless: bool = False
+    chrome_headless: bool = True
